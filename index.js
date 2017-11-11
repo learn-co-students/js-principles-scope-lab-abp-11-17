cd6458e1c61b8e9bab81e86dc313b1593a5893e5
Kyle Loftin <kaloftin@hotmail.com>
2017-11-11 09:53:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  return customerName = "BOB";
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Whatever";
+
+function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = "Blah";
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "Kyle";
+  const favoriteCustomer = "Anyone else";
+}
