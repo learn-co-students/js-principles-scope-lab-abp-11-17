d6ce8a825a798143aac5445bad28c043d74fcd8b
Adil Asif <asifadilanis@gmail.com>
2017-11-10 23:20:39 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+/* jshint esversion: 6 */
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(name) {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(name) {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(name) {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'kerpal';
+
+function changeLeastFavoriteCustomer(name) {
+  leastFavoriteCustomer = 'abtar';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'mike';
+  let favoriteCustomer = 'susan';
+}
