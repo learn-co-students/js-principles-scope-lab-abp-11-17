50fa6ab6299a971ccf0ecfc0daf112fe6eed3342
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-12 06:59:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+//function upperCaseCustomerName(customerName) {
+//  console.log (`${customerName}`.toUpperCase())
+//}
+//function upperCaseCustomerName(customerName) {
+//return `${customerName}`.toUpperCase()
+//}
+function upperCaseCustomerName() {
+customerName = 'BOB'
+}
+
+// in global scope and assigns it to be 'not bob'.
+function setBestCustomer() {
+  bestCustomer= 'not bob'
+}
+
+function overwriteBestCustomer(){
+  bestCustomer= 'maybe bob'
+  }
+
+  const leastFavoriteCustomer = "mike"
+
+  function changeLeastFavoriteCustomer() {
+      leastFavoriteCustomer = "mikey";
+  }
+
+  function attemptTwoFavoriteCustomers(){
+    let FavoriteCustomers= "mikey"
+    let FavoriteCustomers= "mikey2"
+  }
