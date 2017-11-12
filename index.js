25ebb42775709a697d341fbf7082b007030a0efa
Beth Schofield <bethmschofield@gmail.com>
2017-11-12 12:21:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "Magneto"
+
+function changeLeastFavoriteCustomer(string) {
+  leastFavoriteCustomer = string
+}
+
+
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "Beth"
+  let favoriteCustomer = "Aleks"
+}
