1f0b91ed73fd57e1c1dedc0cfdfc9740899e7e0d
Cory West-Forbes <cwestforbes@gmail.com>
2017-11-13 23:04:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'jimmmy';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = '2pac';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'bobby';
+  let favoriteCustomer = 'jim';
+  const favoriteCustomer = 'jimmy';
+}
