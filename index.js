ede1d2aeead4e0e85ae1080ef1c741eeb346e066
Jessica Potter <jessica.potter@state.co.us>
2017-11-16 18:53:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName () {
+  customerName = "BOB"
+}
+
+function setBestCustomer () {
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer () {
+  bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "jessica"
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new bob"
+}
+
+function attemptTwoFavoriteCustomers () {
+  let favoriteCustomer = "Sandy"
+  let favoriteCustomer = "Juan"
+}
