612ac055419bf1ff6bd5954e71a44461257f82df
nancy sin <nsin@sbcglobal.net>
2017-11-19 03:11:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Wrvarite your solution in this file!
+var customerName= ("bob");
+
+function upperCaseCustomerName(){
+  customerName=customerName.toUpperCase()
+}
+ function setBestCustomer(){
+   bestCustomer= 'not bob';
+ }
+
+function overwriteBestCustomer(){
+  bestCustomer= 'maybe bob';
+}
+
+const leastFavoriteCustomer="Zane";
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer= 'Zane';
+}
+
+  function attemptTwoFavoriteCustomers() {
+    let FavoriteCustomers=(Jane); let FavoriteCustomers=(John);
+    }
