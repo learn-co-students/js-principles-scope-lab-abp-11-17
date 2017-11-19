a9c2e34bd470bcb34604c0df7a229057aaca8133
Miriam Castillo <castillo.m.mc@gmail.com>
2017-11-19 02:03:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName=('bob');
+function upperCaseCustomerName() {
+  customerName=customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+bestCustomer='not bob';
+}
+
+function overwriteBestCustomer(){
+  bestCustomer='maybe bob';
+}
+
+const leastFavoriteCustomer='booty';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer='boop';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer='mark';
+  const favoriteCustomer='manny';
+}
