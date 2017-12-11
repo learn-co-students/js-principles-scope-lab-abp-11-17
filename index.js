d25b94ba44f79b323991b86a563ad92508bd58fc
Easwari Ramani <eshuprad@gmail.com>
2017-12-11 14:18:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+var customerName="bob";
+const leastFavoriteCustomer="eshu";
+function upperCaseCustomerName(){
+  customerName= customerName.toUpperCase();
+  return customerName;
+
+}
+function setBestCustomer(){
+  bestCustomer="not bob";
+}
+function overwriteBestCustomer()
+{
+  bestCustomer="maybe bob";
+}
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer="Shreya";
+}
+function attemptTwoFavoriteCustomers()
+{
+  let favoriteCustomer;
+  let favoriteCustomer;
+/*  try{
+
+    throw "SyntaxError";
+  }
+  catch(err){
+    console.log("unsuccessfully tries to declare favoriteCustomer with let two time");
+  }*/
+
+}
