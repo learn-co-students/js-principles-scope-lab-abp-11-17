7621e3d6e55596a5abc647ea094504f1f7751e59
ahotchkin <ahotchk1@yahoo.com>
2019-09-30 09:27:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 // Write your solution in this file!
-var customerName = 'bob'
+var customerName = "bob";
 
-function upperCaseCustomerName(){
+function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
 }
 
-function setBestCustomer(){
-  bestCustomer = 'not bob'
+function setBestCustomer() {
+  bestCustomer = "not bob";
 }
 
-function overwriteBestCustomer(){
-  bestCustomer = 'maybe bob'
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
 }
 
-const leastFavoriteCustomer = 'joe'
+const leastFavoriteCustomer = "also not bob";
 
-function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'sam'
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "or is it bob?";
 }
 
-function attemptTwoFavoriteCustomers(){
-  let favoriteCustomer = 'paul'
-  let favoriteCustomer = 'tom'
+// test isn't passing, but is throwing a SyntaxError, as expected
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "still not bob";
+  let favoriteCustomer = "no way is it bob";
 }
